692a10c174a68aa67ae275f10691a1816aaaf122
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2023-12-08 22:07:44 +0800

    d8p2

--- day8/code.ts
+++ day8/code.ts
@@ -1,7 +1,22 @@
 import { run, print, sum } from "../util.ts";
 // 21:31
 // 21:42 🌟
-// 
+// 22:04 🌟🌟
+function primeFactors(n) {
+  const factors: number[] = [];
+  let divisor = 2;
+
+  while (n >= 2) {
+    if (n % divisor == 0) {
+      factors.push(divisor);
+      n = n / divisor;
+    } else {
+      divisor++;
+    }
+  }
+  return factors;
+}
+
 const main = async (input: string) => {
   // Code goes here
   print("Running");
@@ -11,11 +26,14 @@ const main = async (input: string) => {
     .split("")
     .map((a) => ({ L: 0, R: 1 }[a]));
 
+  const startPoints: string[] = [];
+
   const lookup = ls
     .map((a) => a.trim())
     .filter((a) => !!a)
     .reduce((map, line) => {
       const [from, to] = line.split(" = ");
+      if (from.endsWith("A")) startPoints.push(from);
       return {
         ...map,
         [from]: to.slice(1, -1).split(", "),
@@ -23,15 +41,37 @@ const main = async (input: string) => {
     }, {});
 
   print(lookup);
-  let current = "AAA";
-  let counter = 0;
-  while (current !== "ZZZ") {
-    const d = dirs[counter % dirs.length];
-    print(current, d);
-    current = lookup[current][d];
-    counter++;
-  }
-  return counter;
+
+  // calc the time to do a loop for each starting point
+  let circleTimes = startPoints.map((current) => {
+    let counter = 0;
+    while (!current.endsWith("Z")) {
+      const d = dirs[counter % dirs.length];
+      current = lookup[current][d];
+      counter++;
+    }
+    return counter;
+  });
+
+  // calc the prime factors
+  const c = circleTimes.map(primeFactors).reduce((obj, val) => {
+    const me = {};
+    for (let item of val) {
+      me[item] ||= 0;
+      me[item]++;
+    }
+    for (let a of Object.keys(me)) {
+      obj[a] = Math.max(obj[a] || 0, me[a]);
+    }
+    return obj;
+  }, {});
+
+  // find the LCM of all circleTimes
+  let k = Object.entries(c).reduce(
+    (a, [num, exp]) => a * Math.pow(parseInt(num), exp),
+    1
+  );
+  return k;
 };
 
-await run(main, import.meta.url, "2");
+await run(main, import.meta.url, "6");
